0e4961850c2398d551c920dfe70f801ef2bb8a9c
csjxing <csjxing@163.com>
2013-07-01 11:29:55 +0800

    1

--- code/biz/core/src/main/java/com/doucome/stockop/biz/core/model/NativeStruct.java
+++ code/biz/core/src/main/java/com/doucome/stockop/biz/core/model/NativeStruct.java
@@ -3,26 +3,23 @@
 import com.doucome.stockop.biz.core.utils.NativeConvertUtils;
 
 /**
- * 
  * <ul>
- *  <li>byte</li>	
- *  <li>int</li>
- *  <li>float</li>
- *  <li>long</li>
- *  <li>String</li>
- * 	
+ * <li>byte</li>
+ * <li>int</li>
+ * <li>float</li>
+ * <li>long</li>
+ * <li>String</li>
  * </ul>
+ * 
  * @author langben 2013-6-27
- *
  */
 @SuppressWarnings("serial")
 public abstract class NativeStruct extends AbstractModel {
 
 	/**
-	 * 
 	 * @return
 	 */
 	public byte[] getNativeBuff() {
-		return NativeConvertUtils.object2NativeBuff(this) ;
+		return NativeConvertUtils.object2NativeBuff(this);
 	}
 }
