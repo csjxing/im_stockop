353e10149e6e5d8ca1b4d88fb12f2451664b5386
wei <gw83328@hotmail.com>
2013-07-04 22:46:48 +0800

    sdsdsdsds

---
+++ code/biz/core/src/main/java/com/doucome/stockop/biz/core/model/cpp/Test.java
@@ -0,0 +1,13 @@
+package com.doucome.stockop.biz.core.model.cpp;
+
+public class Test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
